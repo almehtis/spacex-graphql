d8a07826f6d412c6b5d7710f40c37bfbf2dba135
almehtis <esriechtnachwermut@gmail.com>
2023-04-26 09:24:09 +0300

    refactor(src): delete unnecessary memo, edit useState

--- src/common/Header/index.tsx
+++ src/common/Header/index.tsx
@@ -1,14 +1,14 @@
-import { memo, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Tab } from '../Tab'
 import styles from './styles/Header.module.css'
 import { useCookies } from 'react-cookie'
 
-export const Header = memo(function Header() {
-  const [state, setState] = useState(0)
+export const Header = function Header() {
+  const [bookmarks, setBookmarks] = useState(0)
   const [cookies] = useCookies()
 
   useEffect(() => {
-    setState(Object.entries(cookies).length)
+    setBookmarks(Object.entries(cookies).length)
   }, [cookies])
 
   return (
@@ -16,8 +16,8 @@ export const Header = memo(function Header() {
       <h1 className={styles.header}>SpaceX Missions</h1>
       <div className={styles.tabs_block}>
         <Tab href='/' text='Show all' />
-        <Tab href='/bookmarks' text={`Bookmarks ${state}`} />
+        <Tab href='/bookmarks' text={`Bookmarks ${bookmarks}`} />
       </div>
     </div>
   )
-})
+}
